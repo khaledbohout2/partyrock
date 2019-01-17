165abb9287978d432f6b5775704c7de75318279f
Khaled Bohout <amrutaka@MacBook-Pro-alkhas-b-amr.local>
2019-01-17 19:13:52 +0200

    Initial Commit

---
+++ partyrock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  partyrock
+//
+//  Created by Khaled Bohout on 1/17/19.
+//  Copyright © 2019 Khaled Bohout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
